68511969e54365b2a86f7faed02bd560067cf703
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-10-19 20:17:08 +0300

    Added hillclimbing algorithm for solving maze

--- Lab_2/algorithms.py
+++ Lab_2/algorithms.py
@@ -1,5 +1,6 @@
 import ai_operations as ai
 import numpy as np
+import random
 
 
 def solve_bkt(maze: np.ndarray, xs: int, ys: int, xd: int, yd: int) -> (bool, np.ndarray):
@@ -78,6 +79,9 @@ def solve_lee(maze: np.ndarray, xs: int, ys: int, xd: int, yd: int) -> (bool, np
 
 def solve_hill_climbing(maze: np.ndarray, xs: int, ys: int, xd: int, yd: int) -> (bool, np.ndarray):
     """
+    at any possible point, hill climbing chooses one random better solution, evaluating the distance between possible
+    choices and current point, never overestimating the distance
+
     :param maze:
     :param xs:
     :param ys:
@@ -85,4 +89,34 @@ def solve_hill_climbing(maze: np.ndarray, xs: int, ys: int, xd: int, yd: int) ->
     :param yd:
     :return: Boolean and eventual solution
     """
-    return False, maze
+    maze = ai.get_initial_state(maze, xs, ys)
+    dx = [0, 1, 0, -1]
+    dy = [-1, 0, 1, 0]
+    xp, yp = (xs, ys)
+
+    def get_estimated_distance(current_x: int, current_y: int, destination_x: int, destination_y: int) -> int:
+        """
+
+        :param current_x:
+        :param current_y:
+        :param destination_x:
+        :param destination_y:
+        :return: value = calculating and/or underestimating distance between current and destination
+        """
+        return abs(current_x - destination_x) + abs(current_y - destination_y)
+
+    while True:
+        current_value = get_estimated_distance(xp, yp, xd, yd)
+        maze = ai.apply_transition(maze, xp, yp)
+        if ai.is_final(maze, xd, yd):
+            return True, maze
+        possible_choices = []
+        for i in range(4):
+            xp2 = xp + dx[i]
+            yp2 = yp + dy[i]
+            if ai.is_transition_valid(maze, xp2, yp2) and get_estimated_distance(xp2, yp2, xd, yd) <= current_value:
+                possible_choices.append((xp2, yp2))
+        if len(possible_choices) > 0:
+            xp, yp = possible_choices[random.randint(0, len(possible_choices) - 1)]
+        else:
+            return False, maze
