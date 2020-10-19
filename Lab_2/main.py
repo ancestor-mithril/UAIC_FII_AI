84c0d3577c0f0fe38faf6cf209218db52917c1e7
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2020-10-19 21:22:12 +0300

    added simulated annealing algorithm

--- Lab_2/main.py
+++ Lab_2/main.py
@@ -2,7 +2,7 @@ import algorithms as alg
 import ai_operations as ai
 import numpy as np
 
-maze = ai.get_problem_instance(10, 10)
+maze = ai.get_problem_instance(15, 15)
 fd = open("input.txt", "r")
 xs, ys, xd, yd = map(int, fd.read().split())
 
@@ -20,13 +20,15 @@ maze = np.array([
     [0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ])
+# labirint fara bazine de acumulare in minime locale extinse pe toata suprafata de cautare a algoritmului hillclimbing
+
 
 print("Initial maze:")
 print(maze)
 print("Starting position: (" + str(xs) + ", " + str(ys) + ")")
 print("Ending position: (" + str(xd) + ", " + str(yd) + ")")
 
-for algorithm in [alg.solve_bkt, alg.solve_lee]:
+for algorithm in [alg.solve_bkt, alg.solve_lee, alg.solve_hill_climbing, alg.solve_simulated_annealing]:
     print("\nTrying " + str(algorithm) + " algorithm...\n")
     solution_found, final_maze = algorithm(maze, xs, ys, xd, yd)
 
@@ -35,14 +37,14 @@ for algorithm in [alg.solve_bkt, alg.solve_lee]:
         print("Final maze:")
         print(final_maze)
 
-algorithm = alg.solve_hill_climbing
-print("\nTrying " + str(algorithm) + " algorithm...\n")
-for i in range(1000):
-    solution_found, final_maze = algorithm(maze, xs, ys, xd, yd)
-    if solution_found:
-        print("Solution found: " + str(solution_found))
-        print("Final maze:")
-        print(final_maze)
-        break
-else:
-    print("Solution found: " + str(False))
+# algorithm = alg.solve_hill_climbing
+# print("\nTrying " + str(algorithm) + " algorithm...\n")
+# for i in range(1000):
+#     solution_found, final_maze = algorithm(maze, xs, ys, xd, yd)
+#     if solution_found:
+#         print("Solution found: " + str(solution_found))
+#         print("Final maze:")
+#         print(final_maze)
+#         break
+# else:
+#     print("Solution found: " + str(False))
